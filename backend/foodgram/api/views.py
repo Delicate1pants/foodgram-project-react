3bbf6141e432afa9d50956304da7582b00b5932d
Delicate1pants <86769719+Delicate1pants@users.noreply.github.com>
2022-05-04 07:12:29 +0700

    maybe I should start with users

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,10 +2,15 @@ from .mixins import ExcludeUpdate_ModelViewSet
 
 
 class RecipeViewSet(ExcludeUpdate_ModelViewSet):
-    serializer_class = RecipeSerializer
-    permission_classes = (HasAccessOrReadOnly,)
-    pagination_class = PageNumberPagination
-    filter_backends = [RecipeFilter]
+    serializer_class = 'RecipeSerializer'
+    permission_classes = ('HasAccessOrReadOnly',)
+    pagination_class = 'PageNumberPagination'
+
+    filter_backends = ['SearchFilter or DjangoFilterBackend?']
+    filterset_class = '?'
     search_fields = ('something',)
+    search_fields = ['=something', ]
+
     queryset = '?'
+
     lookup_field = '?'
