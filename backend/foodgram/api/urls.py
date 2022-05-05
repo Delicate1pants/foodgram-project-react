8875e03e7e02767b6665b711d942685046222687
Delicate1pants <86769719+Delicate1pants@users.noreply.github.com>
2022-05-05 12:10:58 +0700

    djoser added

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -6,8 +6,9 @@ from .views import RecipeViewSet
 app_name = 'api'
 
 router = routers.SimpleRouter()
-router.register(r'recipes', RecipeViewSet, basename='recipes')
+# router.register(r'recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    # path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
 ]
