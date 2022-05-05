42a40d254989ba98ef9f6e5a0e1c0c160873cfbf
Delicate1pants <86769719+Delicate1pants@users.noreply.github.com>
2022-05-05 13:12:56 +0700

    looks like we are starting over again

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'rest_framework',
     # 'rest_framework_simplejwt',
     'djoser',
-    'api'
+    'api',
+    'recipes',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
