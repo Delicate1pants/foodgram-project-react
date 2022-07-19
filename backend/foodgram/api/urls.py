2fea54ed1652f02917ac24e215492f2a08187b33
Delicate1pants <86769719+Delicate1pants@users.noreply.github.com>
2022-07-19 16:52:41 +0700

    13/41

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -13,34 +13,43 @@ router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'tags', TagViewSet, basename='tags')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 
-urlpatterns = [
+subpaths = [
     path(
-        'v1/users/subscriptions/',
+        'users/subscriptions/',
         SubscriptionViewSet.as_view({'get': 'list'}),
         name='sub-list'
     ),
     path(
-        'v1/users/<int:author_id>/subscribe/',
+        'users/<int:author_id>/subscribe/',
         SubscriptionViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
         name='sub-detail'
     ),
     path(
-        'v1/recipes/<int:recipe_id>/favorite/',
+        'recipes/<int:recipe_id>/favorite/',
         FavouritesViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
         name='favorite'
     ),
     path(
-        'v1/recipes/<int:recipe_id>/shopping_cart/',
+        'recipes/<int:recipe_id>/shopping_cart/',
         ShoppingCartViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
         name='shopping_cart'
     ),
     path(
-        'v1/recipes/download_shopping_cart/',
+        'recipes/download_shopping_cart/',
         ShoppingCartViewSet.as_view({'get': 'list'}),
         name='download_shopping_cart'
     ),
-    path('v1/', include(router.urls)),
-    path('v1/', include('djoser.urls')),
-    path('v1/auth/token/login/', TokenCreateView.as_view()),
-    path('v1/auth/token/logout/', DjoserTokenDestroyView.as_view()),
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/token/login/', TokenCreateView.as_view(), name='token_create'),
+    path(
+        'auth/token/logout/', DjoserTokenDestroyView.as_view(),
+        name='token_destroy'
+    ),
+]
+
+urlpatterns = [
+    path(
+        'v1/', include(subpaths)
+    )
 ]
