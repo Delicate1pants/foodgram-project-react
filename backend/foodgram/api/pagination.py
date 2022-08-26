b7457b7d6b423e71e6a0fafc6e7ab2f0ab515f65
Outer <delicate_pants@practicum-backend.ru-central1.internal>
2022-08-26 23:00:28 +0000

    4 fixes left, v2

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -3,9 +3,3 @@ from rest_framework.pagination import PageNumberPagination
 
 class ListPagination(PageNumberPagination):
     page_size_query_param = 'limit'
-
-
-class IngredientListPagination(PageNumberPagination):
-    # Пагинация не определена спецификацией, но 2188 объект при выдаче
-    # всех ингредиентов - это слишком, я думаю
-    page_size = 50
